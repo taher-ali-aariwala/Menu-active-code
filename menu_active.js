797c15f0ecd908590414bd768ef497e1340d9791
Taher Ali Aariwala <55915071+taher-web@users.noreply.github.com>
2021-12-02 12:25:46 +0530

    Update menu_active.js

--- menu_active.js
+++ menu_active.js
@@ -21,11 +21,23 @@ return this.href == nk;
 // console.log(o)
 if (!o.is("li")) break;
 o = o.parent()
-.addClass("mm-show")
+.addClass("your_class")
 .parent()
 .addClass("active");
 }
 
+// ______________Active Class
+	$(document).ready(function() {
+		$(".horizontalMenu-list li a").each(function() {
+			var pageUrl = window.location.href.split(/[?#]/)[0];
+			if (this.href == pageUrl) {
+				$(this).addClass("active");
+				$(this).parent().addClass("active"); // add active to li of the current link
+				$(this).parent().parent().prev().addClass("active"); // add active class to an anchor
+				$(this).parent().parent().prev().click(); // click the item to make it drop
+			}
+		});
+	});
 
 // ______________Quantity Cart Increase & Descrease
 $(function () {
