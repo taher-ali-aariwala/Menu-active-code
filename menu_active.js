73a67d0b94a1d37e30c4ed40a763d8b79fca083f
Taher Ali Aariwala <55915071+taher-web@users.noreply.github.com>
2021-12-02 12:27:07 +0530

    Update menu_active.js

--- menu_active.js
+++ menu_active.js
@@ -27,17 +27,25 @@ o = o.parent()
 }
 
 // ______________Active Class
-	$(document).ready(function() {
-		$(".horizontalMenu-list li a").each(function() {
-			var pageUrl = window.location.href.split(/[?#]/)[0];
-			if (this.href == pageUrl) {
-				$(this).addClass("active");
-				$(this).parent().addClass("active"); // add active to li of the current link
-				$(this).parent().parent().prev().addClass("active"); // add active class to an anchor
-				$(this).parent().parent().prev().click(); // click the item to make it drop
-			}
-		});
-	});
+$(document).ready(function() {
+$(".horizontalMenu-list li a").each(function() {
+var pageUrl = window.location.href.split(/[?#]/)[0];
+if (this.href == pageUrl) {
+$(this).addClass("active");
+$(this).parent().addClass("active"); // add active to li of the current link
+$(this).parent().parent().prev().addClass("active"); // add active class to an anchor
+$(this).parent().parent().prev().click(); // click the item to make it drop
+}
+});
+});
+
+// ______________ Cover-image
+$(".cover-image").each(function() {
+var attr = $(this).attr('data-image-src');
+if (typeof attr !== typeof undefined && attr !== false) {
+$(this).css('background', 'url(' + attr + ') center center');
+}
+});
 
 // ______________Quantity Cart Increase & Descrease
 $(function () {
